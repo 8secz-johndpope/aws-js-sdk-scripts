cfbf054bb07f2ec7283e06a9c206ce37a0d486b1
Han Li <vcfvct@gmail.com>
2016-09-26 17:36:12 -0400

    seperate the util methods to its own module

--- addHiveStep.js
+++ addHiveStep.js
@@ -1,78 +1,31 @@
 #!/usr/bin/env node
 
 let AWS = require('aws-sdk');
+const exec = require('child_process').exec;
+let awsUtil = require('./awsUtil');
 
 AWS.config.update({ region: 'us-east-1' });
 
 let args = process.argv.slice(2);
 //the first argument should be the stepName and 2nd be the hql file name;
-addHiveStep(args[0], args[1]);
+let stepName = args[0];
+let hqlFile = args[1];
 
-function getStagingBucket() {
-    let s3 = new AWS.S3();
-    const STAGE_BUCKETS = ['application-dev-staging', 'application-qa-staging', 'application-prod-staging'];
-    return new Promise((resolve, reject) => {
-        s3.listBuckets(function(err, data) {
-            if (err) {
-                console.log(err, err.stack);
-                return reject(err);
-            } else {
-                let targetBucket = data.Buckets.find((bucket) => STAGE_BUCKETS.some((stageBucket) => bucket.Name.indexOf(stageBucket) >= 0));
-                console.log(`targetBucket : ${targetBucket.Name}`);
-                return resolve(targetBucket.Name);
-            }
-        });
+const RC_CLUSTER_NAME = 'RC.HIVE.ADHOC';
+
+Promise.all([awsUtil.getClusterId(RC_CLUSTER_NAME), awsUtil.getStagingBucket()]).then((values) => {
+    let clusterId = values[0];
+    let bucketName = values[1];
+    const env = awsUtil.getEnvFromBucketName(bucketName);
+    const fileLocation = 's3://' + bucketName + '/RC/' + env + '/hive-scripts/' + hqlFile;
+    let cmd = 'aws emr add-steps --cluster-id ' + clusterId + ' --steps Type=HIVE,Name="' + stepName + '",ActionOnFailure=CONTINUE,Args=[-f,' + fileLocation + ']';
+    console.log(`Command to execute: ${cmd}`);
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
     });
-}
-
-function getClusterId() {
-    let emr = new AWS.EMR();
-    let params = { ClusterStates: ['RUNNING', 'WAITING'] };
-    const CLUSTER_NAME = 'RC.HIVE.ADHOC';
-
-    return new Promise((resolve, reject) => {
-        emr.listClusters(params, function(err, data) {
-            if (err) {
-                console.log(err, err.stack);
-                return reject(err);
-            } else {
-                let targetEmr = data.Clusters.find((ins) => ins.Name === CLUSTER_NAME);
-                console.log(`targetEmr : ${targetEmr.Id}`);
-                return resolve(targetEmr.Id);
-            }
-        });
-    });
-}
-
-function addHiveStep(stepName, hqlFile) {
-    const exec = require('child_process').exec;
-    Promise.all([getClusterId(), getStagingBucket()]).then((values) => {
-        let clusterId = values[0];
-        let bucketName = values[1];
-        const env = getEnvFromBucketName(bucketName);
-        const fileLocation = bucketName + '/RC/' + env + '/hive-scripts/' + hqlFile;
-        let cmd = 'aws emr add-steps --cluster-id ' + clusterId + ' --steps Type=HIVE,Name="' + stepName + '",ActionOnFailure=CONTINUE,Args=[-f,' + fileLocation + ']';
-        exec('echo ' + cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-        });
-    });
-
-}
-
-function getEnvFromBucketName(bucketName) {
-    if (bucketName.indexOf('dev') >= 0) {
-        return 'DEV';
-    }
-    if (bucketName.indexOf('qa') >= 0) {
-        return 'QA';
-    }
-    if (bucketName.indexOf('prod') >= 0) {
-        return 'PROD';
-    }
-    throw new Error('unsupported env for bucket: ' + bucketName);
-}
+});
--- clusterList.js
+++ clusterList.js
@@ -2,14 +2,24 @@
 
 var AWS = require('aws-sdk');
 
-AWS.config.update({region: 'us-east-1'});
+AWS.config.update({ region: 'us-east-1' });
 var emr = new AWS.EMR();
 
-var params = {ClusterStates: ['RUNNING','WAITING']};
-emr.listClusters(params, function(err, data){
-  if (err) console.log(err, err.stack); // an error occurred
-  else {
-        var rcEmr = data.Clusters.find((ins) => ins.Name === 'RC.HIVE.ADHOC');
-        console.log(rcEmr);
-  }
-});
+var params = { ClusterStates: ['RUNNING', 'WAITING'] };
+
+function getEmr(params) {
+    emr.listClusters(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            var rcEmr = data.Clusters.find((ins) => ins.Name === 'RC.HIVE.ADHOC');
+            if (!rcEmr && data.Marker) {
+                return getEmr({Marker: data.Marker})
+            } else {
+                console.log(rcEmr);
+                return rcEmr;
+            }
+        }
+    });
+}
+
+getEmr(params);
