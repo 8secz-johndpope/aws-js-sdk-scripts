d8ce997040c07f57243ce34d6b4d143a67ffa325
Han Li <vcfvct@gmail.com>
2016-10-02 19:13:37 -0400

    move create code to util

--- addNewCluster.js
+++ addNewCluster.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
 let AWS = require('aws-sdk');
-const exec = require('child_process').exec;
 let awsUtil = require('./lib/awsUtil');
 
 AWS.config.update({ region: 'us-east-1' });
 
+let emr = new AWS.EMR();
+
 let args = process.argv.slice(2);
 //the first argument should be the stepName and 2nd be the hql file name;
-let stepName = args[0];
-let hqlFile = args[1];
+//let stepName = args[0];
 
 const RC_CLUSTER_NAME = 'RC.HIVE.ADHOC';
 
@@ -22,48 +22,10 @@ Promise.all([awsUtil.getClusterId(RC_CLUSTER_NAME), awsUtil.getStagingBucket()])
             console.log(`Cluster ${RC_CLUSTER_NAME} already exist! Quit`);
         } else {
             const env = awsUtil.getEnvFromBucketName(bucketName);
-            let newClusterParams = {
-                instances: {
-                    AdditionalMasterSecurityGroups: [
-                        'sg-17746a70', 'sg-8d79e1f4'
-                    ],
-                    Ec2KeyName: 'APP_RC_DEV',
-                    Ec2SubnetId: 'subnet-56a99422',
-                    EmrManagedMasterSecurityGroup: 'sg-cb17b1ae',
-                    EmrManagedSlaveSecurityGroup: 'sg-d417b1b1',
-                    InstanceGroups: [{
-                        InstanceCount: 1,
-                        InstanceRole: 'MASTER',
-                        InstanceType: 'm3.2xlarge',
-                        BidPrice: '1',
-                        Market: 'SPOT',
-                        Name: 'Master-Node'
-                    }, {
-                        InstanceCount: 1,
-                        InstanceRole: 'CORE',
-                        InstanceType: 'm3.xlarge',
-                        BidPrice: '1',
-                        Market: 'SPOT',
-                        Name: 'Slave-Node'
-                    }],
-                    KeepJobFlowAliveWhenNoSteps: true,
-
-                },
-                Name: RC_CLUSTER_NAME,
-                AmiVersion: '3.9.0',
-                Tags: [{
-                    Key: 'Cost Center',
-                    Value: 'SCR901'
-                }, {
-                    Key: 'AGS',
-                    Value: 'RC'
-                }]
-            };
-            console.log('creating new cluster with config: ' + JSON.Stringify(newClusterParams));
-            emr.runJobFlow(newClusterParams, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else console.log(data); // successful response
-            });
-
+            return awsUtil.createCluster(RC_CLUSTER_NAME);
         }
-    });
+    }).then((newClusterId) => {
+    if (newClusterId) {
+        console.log(`New cluster created, ID: ${clusterId}`);
+    }
+});
