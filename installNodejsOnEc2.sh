cc99eeb340c42f9d5a15e2817977c4230194009f
Han Li <vcfvct@gmail.com>
2016-11-23 18:18:34 -0500

    modify the install node so that it check whehter nodejs is there first

--- installNodejsOnEc2.sh
+++ installNodejsOnEc2.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo  bash -
-sudo yum -y install nodejs
+if  node -v > /dev/null 
+then
+  echo "node is installed, skipping..."
+else
+  curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo  bash -
+  sudo yum -y install nodejs
+fi
 
 npm install
